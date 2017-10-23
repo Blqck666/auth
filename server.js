37f2c9830485d7c6c720cc1262d05254e1332ffe
aymen <atlili@wizzlabs.com>
2017-10-23 12:38:50 +0100

    add login and all server

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-var port = process.env.PORT || 5057;
-var IPADDRESS = process.env.IP || '127.0.0.1';
-var port2 = 3001;
-
-var socket2 = require('socket.io')(port2);
-var socket = require('socket.io')(port);
-
+var port = process.env.OPENSHIFT_NODEJS_PORT || 5057;
+var IPADDRESS = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var io = require('socket.io')(port);
 var shortId = require('shortid');
-const MongoClient = require('mongodb').MongoClient;
+
+
+
 
 var players = [];
-var thisPlayerId;
+
 var playerSpeed = 3;
 
-var db;
-var roomId;
-var room = shortId.generate();
+
+
+
+
+console.log('3');
+
+const MongoClient = require('mongodb').MongoClient;
+
+
 
 
 
@@ -23,20 +27,44 @@ console.log("Server started at port : " + port);
 
 
 
-socket.on('connection', function (socket) 
-{
-    console.log("client Unity connected ");
+io.on('connection', function (socket) {
+    var roomDistance;
+    var thisPlayerId = shortId.generate();
+    var player = {
+        sockid:socket.id,
+        id:thisPlayerId,
+        destination:{
+        x:0,
+        y:0    
+        },
+        lastPosition:{
+            x:0,
+            y:0
+        },
+        lastMoveTime : 0,
+        lat : 0,
+        lan : 0,
+        name : ""
+    };
+    players[thisPlayerId] = player;
+    
+    console.log("client connected, id = ", thisPlayerId);
+   
+    socket.emit('register', {id:thisPlayerId});
+    socket.broadcast.emit('spawn', {id:thisPlayerId});
+    socket.broadcast.emit('requestPosition');
+    
+    for(var playerId in players){
+        if(playerId == thisPlayerId)
+            continue;
+        socket.emit('spawn', players[playerId]);
+    };
 
-
-    socket.on('registerChat', function(data)
-    {
-        roomId =  data.rev.id+data.me.id
-            socket.join(roomId);
-                console.log("client Unity Joint room ", data.rev.id+data.me.id);
-                    socket.broadcast.to(roomId).emit('successReg',roomId);
-    });
-
-
+        //hathi tab3eth marra kahw wa9ti player yconnecti
+        //lazem kif lplayer yconnecti w yo9res 3la location ya3mel verification w yab3eth spawn ll player jdid
+        //hathi lazem twali automatique y7el lapp yestana 1 sec yconnecti m3aha location
+   
+    
     socket.on('login', function(data)
     {
         console.log(data.id);
@@ -68,64 +96,157 @@ socket.on('connection', function (socket)
        
         //socket.broadcast.emit('send',data);
     });
+  
+    
+    
+    socket.on('move', function (data) {
+        data.id = thisPlayerId;
+        //console.log('client moved', JSON.stringify(data));
+        
+        player.destination.x = data.d.x;
+        player.destination.y = data.d.y;
+        
+        var elapsedTime = Date.now() - player.lastMoveTime;
+        
+        var travelDistanceLimit = elapsedTime * playerSpeed / 1000;
+        
+        var requestedDistanceTraveled = lineDistance(player.lastPosition, data.c);
+        
+        player.lastMoveTime = Date.now();
+        
+        player.lastPosition = data.c;
+        
+        delete data.c;
+        
+        data.x = data.d.x;
+        data.y = data.d.y;
+        
+        delete data.d;
 
- 
+/*for(var playerId in players){
+        if(playerId == thisPlayerId){
+             continue;
+        }
+            
+            var distance = distanceInKmBetweenEarthCoordinates(players[thisPlayerId].lat,players[thisPlayerId].lan,players[playerId].lat,players[playerId].lan);
+             
+             if(distance < 20)
+             {
+              //  console.log(distance +' MOVE function');
+                    socket.broadcast.emit('move', data);
+             }
+        };*/
 
-    socket.on('send', function(data)
-    {
-        socket.broadcast.to(roomId).emit('send',data);
-            console.log("broadcast ", roomId);
+        socket.broadcast.emit('move', data);
 
-                MongoClient.connect('mongodb://pokemap:fucksatan001@ds032887.mlab.com:32887/pokemap', function(err, db) 
-                {
-                    if (err) throw err;
-                        var myobj = { idSend: data.id, msg: data.msg ,idRes : data.res , idroom:data.room};
-                            db.collection("chat").insertOne(myobj, function(err, res) 
-                            {
-                                if (err) throw err;
-                                console.log("1 record inserted");
-   
-                            });
+        
+        //var distance =  distanceInKmBetweenEarthCoordinates(player.lat,player.lan,0,0);
+        //console.log(distance);
+        //players[thisPlayerId].lat
+        //console.log('localisation : ', JSON.stringify(players[thisPlayerId].lat));
+        
+    });
+
+    socket.on('initloc',function(data){
+        console.log('Tba3thet INIT : /////////////////////////////////////////////////////////////////////');
+        data.id = thisPlayerId;
+        player.lat = data.x;
+        player.lan = data.y;
+        
        
-                    //socket.broadcast.emit('send',data);
-                });
-
-
+        console.log('INIT Wfet ');
     });
 
-    
-    socket.on('disconnect', function () 
-    {
-            console.log('client disconected');
-                delete players[thisPlayerId];
-                    socket.emit('disconnected', {id:thisPlayerId});
-    });
-
-});
 
 
-socket2.on('connect', function (socket2) 
-{
-    
-        console.log("server started on port " + port2 +" And recived a Connection");
-    
-    socket2.on("userS",function(data)
-    {
-        thisPlayerId=data.arrayToSendToBrowser.id
+
+
+
+
+
+socket.on('localisation',function(data){
+        data.id = thisPlayerId;
+        player.lat = data.x;
+        player.lan = data.y;
         
-            players[thisPlayerId] = data.arrayToSendToBrowser;
-                console.log(players[thisPlayerId].sockid);
-        
+        //console.log('localisation : ', JSON.stringify(data));
     });
 
 
+    socket.on('rotate',function(data){
+        data.id = thisPlayerId;
+        data.y = data.rot.y;
+        data.w = data.rot.w;
+        delete data.rot;
+        //console.log('client rotated',JSON.stringify(data));
+        socket.broadcast.emit('rotate',data);
+    });
+    
+    socket.on('send', function(data){
+        data.id = thisPlayerId;
 
+        console.log('the player ' + data.id + 'Has Send this msg : ' + JSON.stringify(data.msg) + " To This Player : " + data.rec + "with id = : "+players[data.rec].sockid);
+        data.msg = data.msg;
+        data.id = data.id;
+        data.rec = data.rec;
+        io.to(players[data.rec].sockid).emit('send', data);
+        //socket.broadcast.emit('send',data);
+    });
 
-    socket2.on('disconnect', function () 
-    {
+     socket.on('follow', function (data) {
+        data.id = thisPlayerId;
+        console.log("follow request: ", data);
+        socket.broadcast.emit('follow', data);
+    });
+    
+    socket.on('updatePosition', function (data) {
+        console.log("update position: ", data);
+        data.id = thisPlayerId;
+        socket.broadcast.emit('updatePosition', data);
+    });
+    
+    socket.on('attack', function (data) {
+        console.log("attack request: ", data);
+        data.id = thisPlayerId;
+        io.emit('attack', data);
+    });
+    
+    socket.on('disconnect', function () {
         console.log('client disconected');
-            delete players[thisPlayerId];
-                socket2.emit('disconnected', {id:thisPlayerId});
+        delete players[thisPlayerId];
+        socket.broadcast.emit('disconnected', {id:thisPlayerId});
     });
-
 });
+
+function lineDistance(vectorA, vectorB) {
+    var xs = 0;
+    var ys = 0;
+    
+    xs = vectorB.x - vectorA.x;
+    xs = xs * xs;
+    
+    ys = vectorB.y - vectorA.y;
+    ys = ys * ys;
+    
+    return Math.sqrt(xs + ys);
+}
+
+
+function degreesToRadians(degrees) {
+  return degrees * Math.PI / 180;
+}
+
+function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+  var earthRadiusKm = 6371;
+
+  var dLat = degreesToRadians(lat2-lat1);
+  var dLon = degreesToRadians(lon2-lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+          Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  return earthRadiusKm * c;
+}
